7d20c648f73c25aac7d34410d415e1df801a9601
Diego Javier <dijaq08@gmail.com>
2019-03-07 01:01:24 -0500

    show window

--- practice.cpp
+++ practice.cpp
@@ -33,6 +33,40 @@ int main()
         return -1;
     }
 
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+
+    window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+
+    if(window == NULL)
+    {
+    	cout << "NO se puede abrir la ventana GLDW" << endl;
+    	glfwTerminate();
+		return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+
+    if(glewInit() != GLEW_OK)
+    {
+    	cout << "NO inicio GLEW" << endl;
+    	return -1;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do{
+    	glfwSwapBuffers(window);
+    	glfwPollEvents();
+    }
+    while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+
     return 0;
 }
 
